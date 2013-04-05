a1041f127cc2261f3193fe8e3fb31bcca04da996
Justin Joy <justin.joy.9to5@gmail.com>
2013-04-05 17:13:04 +0900

    modified caps for fakevdec

---
+++ gst/compat/gstfdcaps.h
@@ -0,0 +1,45 @@
+/* GStreamer Lightweight Plugins
+ * Copyright (C) 2013 LG Electronics.
+ *	Author : Justin Joy <justin.joy.9to5@gmail.com> 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __GST_FD_CAPS_H__
+#define __GST_FD_CAPS_H__
+
+G_BEGIN_DECLS
+
+#define FD_CAPS \
+        "video/x-divx;" \
+        "video/x-h264;" \
+        "video/x-intel-h263;" \
+        "video/x-h263;" \
+        "video/mpeg;" \
+        "video/x-wmv;" \
+        "video/x-msmpeg;" \
+        "video/x-pn-realvideo;" \
+        "video/x-pn-realvideo;" \
+        "video/x-svq;" \
+        "video/x-ffv;" \
+        "video/x-3ivx;" \
+        "video/x-vp8;" \
+        "video/x-xvid;" \
+        "video/x-flash-video"
+
+G_END_DECLS
+
+#endif // __GST_FD_CAPS_H__
