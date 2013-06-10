352e6a7f594ed4ebb22c520a46a7fbf5d5bc730e
Jeongseok Kim <jeongseok.kim@lge.com>
2013-06-10 20:20:41 +0900

    fcbin: Fix indentation
    DCO-1.0-Signed-off-by: Jeongseok Kim <jeongseok.kim@lge.com>
    
    Change-Id: I2782f1bc857e032811d6f02f0880fa9c66ed2b61
    Reviewed-on: https://gpro.palm.com/4374
    Reviewed-by: Jeongseok Kim <jeongseok.kim@lge.com>
    Tested-by: Jeongseok Kim <jeongseok.kim@lge.com>

--- gst/compat/gstfcbin.c
+++ gst/compat/gstfcbin.c
@@ -37,7 +37,8 @@ GST_DEBUG_CATEGORY_STATIC (fc_bin_debug);
 G_DEFINE_TYPE (GstFCBin, gst_fc_bin, GST_TYPE_BIN);
 
 static void gst_fc_bin_finalize (GObject * obj);
-static GstPad * gst_fc_bin_request_new_pad (GstElement *element, GstPadTemplate *templ, const gchar* name, const GstCaps *caps);
+static GstPad *gst_fc_bin_request_new_pad (GstElement * element,
+    GstPadTemplate * templ, const gchar * name, const GstCaps * caps);
 static gboolean array_has_value (const gchar * values[], const gchar * value);
 
 static GstStaticPadTemplate gst_fc_bin_sink_pad_template =
@@ -120,8 +121,9 @@ gst_fc_bin_class_init (GstFCBinClass * klass)
       "data flow controller behind Fake Decoder",
       "Justin Joy <justin.joy.9to5@gmail.com, Wonchul Lee <wonchul86.lee@lge.com>");
 
-	//element_class->change_state = GST_DEBUG_FUNCPTR (gst_fc_bin_change_state);
-	element_class->request_new_pad = GST_DEBUG_FUNCPTR (gst_fc_bin_request_new_pad);
+  //element_class->change_state = GST_DEBUG_FUNCPTR (gst_fc_bin_change_state);
+  element_class->request_new_pad =
+      GST_DEBUG_FUNCPTR (gst_fc_bin_request_new_pad);
 
   GST_DEBUG_CATEGORY_INIT (fc_bin_debug, "fcbin", 0, "Flow Controller");
 }
@@ -131,13 +133,14 @@ gst_fc_bin_init (GstFCBin * fcbin)
 {
   GST_DEBUG_OBJECT (fcbin, "initializing");
 
-	fcbin->select[GST_FC_BIN_STREAM_AUDIO].channels = g_ptr_array_new ();
-	fcbin->select[GST_FC_BIN_STREAM_AUDIO].media_list[0] = "audio/";
-	fcbin->select[GST_FC_BIN_STREAM_VIDEO].channels = g_ptr_array_new ();
+  fcbin->select[GST_FC_BIN_STREAM_AUDIO].channels = g_ptr_array_new ();
+  fcbin->select[GST_FC_BIN_STREAM_AUDIO].media_list[0] = "audio/";
+  fcbin->select[GST_FC_BIN_STREAM_VIDEO].channels = g_ptr_array_new ();
   fcbin->select[GST_FC_BIN_STREAM_VIDEO].media_list[0] = "video/";
-	fcbin->select[GST_FC_BIN_STREAM_TEXT].channels = g_ptr_array_new ();
+  fcbin->select[GST_FC_BIN_STREAM_TEXT].channels = g_ptr_array_new ();
   fcbin->select[GST_FC_BIN_STREAM_TEXT].media_list[0] = "text/";
-  fcbin->select[GST_FC_BIN_STREAM_TEXT].media_list[1] = "application/x-subtitle";
+  fcbin->select[GST_FC_BIN_STREAM_TEXT].media_list[1] =
+      "application/x-subtitle";
   fcbin->select[GST_FC_BIN_STREAM_TEXT].media_list[2] = "application/x-ssa";
   fcbin->select[GST_FC_BIN_STREAM_TEXT].media_list[3] = "application/x-ass";
   fcbin->select[GST_FC_BIN_STREAM_TEXT].media_list[4] = "subpicture/x-dvd";
@@ -154,82 +157,82 @@ gst_fc_bin_finalize (GObject * obj)
 }
 
 static GstPad *
-gst_fc_bin_request_new_pad (GstElement *element, GstPadTemplate *templ,
-		const gchar* name, const GstCaps *caps)
+gst_fc_bin_request_new_pad (GstElement * element, GstPadTemplate * templ,
+    const gchar * name, const GstCaps * caps)
 {
-	GstFCBin *fcbin;
-	const GstStructure *s;
-	const gchar* in_name;
-  gint i,j;
-	GstFCSelect *selected;
-	GstPad *sinkpad, *ghost_sinkpad;
-	GstCaps *in_caps;
+  GstFCBin *fcbin;
+  const GstStructure *s;
+  const gchar *in_name;
+  gint i, j;
+  GstFCSelect *selected;
+  GstPad *sinkpad, *ghost_sinkpad;
+  GstCaps *in_caps;
 
 
-	fcbin = GST_FC_BIN (element);
-	
-	s = gst_caps_get_structure (caps, 0);
-	in_name = gst_structure_get_name (s);
+  fcbin = GST_FC_BIN (element);
 
-	for (i = 0; i < GST_FC_BIN_STREAM_LAST; i++) {
-		for (j = 0; fcbin->select[i].media_list[j]; j++) {
-			if (array_has_value (fcbin->select[i].media_list, in_name))
-			{
-				selected = &fcbin->select[i];
-				break;
-			}
-		}
-	}	
-	
-	if (selected == NULL)
-		goto unknown_type;
+  s = gst_caps_get_structure (caps, 0);
+  in_name = gst_structure_get_name (s);
 
-	if (selected->selector == NULL) {
-		selected->selector = gst_element_factory_make ("input-selector", NULL);
+  for (i = 0; i < GST_FC_BIN_STREAM_LAST; i++) {
+    for (j = 0; fcbin->select[i].media_list[j]; j++) {
+      if (array_has_value (fcbin->select[i].media_list, in_name)) {
+        selected = &fcbin->select[i];
+        break;
+      }
+    }
+  }
 
-		if (selected->selector == NULL) {
-     /* gst_element_post_message (GST_ELEMENT_CAST (fcbin), 
-          gst_missing_element_message_new (GST_ELEMENT_CAST (fcbin),
-              "input-selector"));*/
-		} else {
+  if (selected == NULL)
+    goto unknown_type;
 
-			g_object_set (selected->selector, "sync-streams", TRUE, NULL);
+  if (selected->selector == NULL) {
+    selected->selector = gst_element_factory_make ("input-selector", NULL);
 
-			gst_bin_add (GST_BIN_CAST(fcbin), selected->selector);
-			gst_element_set_state (selected->selector, GST_STATE_PAUSED);
-		}
-	}
+    if (selected->selector == NULL) {
+      /* gst_element_post_message (GST_ELEMENT_CAST (fcbin), 
+         gst_missing_element_message_new (GST_ELEMENT_CAST (fcbin),
+         "input-selector")); */
+    } else {
 
-	if (selected->srcpad == NULL) {
-		if (selected->selector) {
-			GstPad *sel_srcpad;
+      g_object_set (selected->selector, "sync-streams", TRUE, NULL);
 
-			sel_srcpad = gst_element_get_static_pad (selected->selector, "src");
-			selected->srcpad = gst_ghost_pad_new (NULL, sel_srcpad);
-			gst_pad_set_active (selected->srcpad, TRUE);
-			gst_element_add_pad (element, selected->srcpad);
-		}
-	}
+      gst_bin_add (GST_BIN_CAST (fcbin), selected->selector);
+      gst_element_set_state (selected->selector, GST_STATE_PAUSED);
+    }
+  }
 
-	if (selected->selector) {
-		gchar *pad_name = g_strdup_printf("sink_%d", selected->channels->len);
-		if ((sinkpad = gst_element_get_request_pad (selected->selector, pad_name))) {
-			g_object_set_data (G_OBJECT(sinkpad), "fcbin.select", selected);
+  if (selected->srcpad == NULL) {
+    if (selected->selector) {
+      GstPad *sel_srcpad;
 
-			ghost_sinkpad = gst_ghost_pad_new (NULL, sinkpad);
-			gst_pad_set_active (ghost_sinkpad, TRUE);
-			gst_element_add_pad (element, ghost_sinkpad);
-			g_object_set_data (G_OBJECT(ghost_sinkpad), "fcbin.srcpad", selected->srcpad);
-			
-			g_ptr_array_add (selected->channels, sinkpad);
-		}
-	}
+      sel_srcpad = gst_element_get_static_pad (selected->selector, "src");
+      selected->srcpad = gst_ghost_pad_new (NULL, sel_srcpad);
+      gst_pad_set_active (selected->srcpad, TRUE);
+      gst_element_add_pad (element, selected->srcpad);
+    }
+  }
+
+  if (selected->selector) {
+    gchar *pad_name = g_strdup_printf ("sink_%d", selected->channels->len);
+    if ((sinkpad = gst_element_get_request_pad (selected->selector, pad_name))) {
+      g_object_set_data (G_OBJECT (sinkpad), "fcbin.select", selected);
+
+      ghost_sinkpad = gst_ghost_pad_new (NULL, sinkpad);
+      gst_pad_set_active (ghost_sinkpad, TRUE);
+      gst_element_add_pad (element, ghost_sinkpad);
+      g_object_set_data (G_OBJECT (ghost_sinkpad), "fcbin.srcpad",
+          selected->srcpad);
+
+      g_ptr_array_add (selected->channels, sinkpad);
+    }
+  }
 
 unknown_type:
-	GST_ERROR_OBJECT (fcbin, "unknown type %s for pad %s", name);
+  GST_ERROR_OBJECT (fcbin, "unknown type for pad %s", name);
 
 done:
-	return ghost_sinkpad;
+  return ghost_sinkpad;
 }
 
 //FIXME need to implement resource deallocation code.
@@ -252,13 +255,13 @@ gst_fc_bin_release_pad (GstElement * element, GstPad *pad)
 }
 */
 static gboolean
-array_has_value (const gchar * values[], const gchar * value) 
+array_has_value (const gchar * values[], const gchar * value)
 {
   gint i;
 
-  for (i = 0; values[i]; i++) {  
+  for (i = 0; values[i]; i++) {
     if (g_str_has_prefix (value, values[i]))
       return TRUE;
-  }    
+  }
   return FALSE;
 }
