c57466e8f545c2d242c8b91fa2bfb3f96b826fc3
hoonhee.lee <hoonhee.lee@lge.com>
2014-02-10 16:53:37 +0900

    httpextbin: Add test case for missing plugin.
    When uri is set with an unsupported protocol such as "http+unsupported://",
    httpextbin will give up building an internal source chain.
    If httpextbin detects this case, "missing-plugin" message will be posted to
    the upper layer.
    
    Change-Id: Idb419704dc718e65a0568acbaf5c7f35e9a7e26c
    Reviewed-on: https://gpro.palm.com/29396
    Reviewed-by: Jeongseok Kim <jeongseok.kim@lge.com>
    Tested-by: Jeongseok Kim <jeongseok.kim@lge.com>

--- tests/check/elements/httpextbin.c
+++ tests/check/elements/httpextbin.c
@@ -75,6 +75,59 @@ GST_START_TEST (test_set_uri)
 
 GST_END_TEST;
 
+GST_START_TEST (test_missing_plugin)
+{
+  GstElement *httpextbin;
+  GstBus *bus;
+  GstMessage *msg;
+  GError *err = NULL;
+
+  httpextbin = gst_element_factory_make ("httpextbin", NULL);
+  fail_unless (httpextbin != NULL, "Could not create httpextbin element");
+
+  // set uri as "http+unsupported://"
+  g_object_set (httpextbin, "uri", "http+unsupported://", NULL);
+
+  fail_unless_equals_int (gst_element_set_state (httpextbin, GST_STATE_READY),
+      GST_STATE_CHANGE_SUCCESS);
+  fail_unless_equals_int (gst_element_set_state (httpextbin, GST_STATE_PAUSED),
+      GST_STATE_CHANGE_FAILURE);
+
+  /* there should be at least a missing-plugin message on the bus now and an
+   * error message; the missing-plugin message should be first */
+  bus = gst_element_get_bus (httpextbin);
+
+  msg = gst_bus_poll (bus, GST_MESSAGE_ELEMENT | GST_MESSAGE_ERROR, -1);
+  fail_unless_equals_int (GST_MESSAGE_TYPE (msg), GST_MESSAGE_ELEMENT);
+  s = (GstStructure *) gst_message_get_structure (msg);
+  fail_unless (s != NULL);
+  fail_unless (gst_structure_has_name (s, "missing-plugin"));
+  fail_unless (gst_structure_has_field_typed (s, "detail", G_TYPE_STRING));
+  fail_unless_equals_string (gst_structure_get_string (s, "detail"),
+      "http+unsupported");
+  fail_unless (gst_structure_has_field_typed (s, "type", G_TYPE_STRING));
+  fail_unless_equals_string (gst_structure_get_string (s, "type"), "urisource");
+  gst_message_unref (msg);
+
+  msg = gst_bus_poll (bus, GST_MESSAGE_ERROR, -1);
+  fail_unless_equals_int (GST_MESSAGE_TYPE (msg), GST_MESSAGE_ERROR);
+
+  /* make sure the error is a CORE MISSING_PLUGIN one */
+  gst_message_parse_error (msg, &err, NULL);
+  fail_unless (err != NULL);
+  fail_unless (err->domain == GST_CORE_ERROR, "error has wrong error domain "
+      "%s instead of core-error-quark", g_quark_to_string (err->domain));
+  fail_unless (err->code == GST_CORE_ERROR_MISSING_PLUGIN, "error has wrong "
+      "code %u instead of GST_CORE_ERROR_MISSING_PLUGIN", err->code);
+  g_error_free (err);
+  gst_message_unref (msg);
+  gst_object_unref (bus);
+
+  gst_element_set_state (httpextbin, GST_STATE_NULL);
+
+  gst_object_unref (httpextbin);
+}
+
 static Suite *
 httpextbin_suite (void)
 {
@@ -84,6 +137,8 @@ httpextbin_suite (void)
   tc_chain = tcase_create ("general");
   tcase_add_test (tc_chain, test_uri_interface);
   tcase_add_test (tc_chain, test_set_uri);
+  tcase_add_test (tc_chain, test_missing_plugin);
+
   suite_add_tcase (s, tc_chain);
 
   return s;
