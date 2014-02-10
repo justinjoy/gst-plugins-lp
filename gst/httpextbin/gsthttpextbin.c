0e883de11e2186fe5face87d270d6cbbe65dd66e
hoonhee.lee <hoonhee.lee@lge.com>
2014-02-10 22:32:00 +0900

    httpextbin: Add uri property
    httpextbin stores uri which will be set a real source element
    before paused state.
    
    Change-Id: I9027a8451f2c4a7f8294524e39df7845bc86613a
    Reviewed-on: https://gpro.palm.com/29542
    Reviewed-by: Jeongseok Kim <jeongseok.kim@lge.com>
    Tested-by: Jeongseok Kim <jeongseok.kim@lge.com>

--- gst/httpextbin/gsthttpextbin.c
+++ gst/httpextbin/gsthttpextbin.c
@@ -33,6 +33,14 @@ GST_DEBUG_CATEGORY_STATIC (http_ext_bin_debug);
 
 #define parent_class gst_http_ext_bin_parent_class
 
+#define DEFAULT_PROP_URI NULL
+enum
+{
+  PROP_0,
+  PROP_URI,
+  PROP_LAST
+};
+
 static void gst_http_ext_bin_set_property (GObject * object, guint prop_id,
     const GValue * value, GParamSpec * pspec);
 static void gst_http_ext_bin_get_property (GObject * object, guint prop_id,
@@ -58,6 +66,10 @@ gst_http_ext_bin_class_init (GstHttpExtBinClass * klass)
   gobject_class->get_property = gst_http_ext_bin_get_property;
   gobject_class->finalize = gst_http_ext_bin_finalize;
 
+  g_object_class_install_property (gobject_class, PROP_URI,
+      g_param_spec_string ("uri", "URI",
+          "URI to be set source element",
+          NULL, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
   gst_element_class_set_static_metadata (gstelement_class,
       "Extended Http Source Bin", "Source/Bin/Network/Protocol",
       "HTTP Source with unified data processing element",
@@ -70,6 +82,9 @@ gst_http_ext_bin_class_init (GstHttpExtBinClass * klass)
 static void
 gst_http_ext_bin_init (GstHttpExtBin * bin)
 {
+  /* init member variable */
+  bin->uri = g_strdup (DEFAULT_PROP_URI);
+
   GST_OBJECT_FLAG_SET (bin, GST_ELEMENT_FLAG_SOURCE);
 }
 
@@ -77,7 +92,20 @@ static void
 gst_http_ext_bin_set_property (GObject * object, guint prop_id,
     const GValue * value, GParamSpec * pspec)
 {
+  GstHttpExtBin *bin = GST_HTTP_EXT_BIN (object);
   switch (prop_id) {
+    case PROP_URI:
+    {
+      GST_OBJECT_LOCK (bin);
+      GstURIHandler *handler;
+      GstURIHandlerInterface *iface;
+
+      handler = GST_URI_HANDLER (bin);
+      iface = GST_URI_HANDLER_GET_INTERFACE (handler);
+      iface->set_uri (handler, g_value_get_string (value), NULL);
+      GST_OBJECT_UNLOCK (bin);
+    }
+      break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -88,7 +116,11 @@ static void
 gst_http_ext_bin_get_property (GObject * object, guint prop_id,
     GValue * value, GParamSpec * pspec)
 {
+  GstHttpExtBin *bin = GST_HTTP_EXT_BIN (object);
   switch (prop_id) {
+    case PROP_URI:
+      g_value_set_string (value, bin->uri);
+      break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -98,6 +130,10 @@ gst_http_ext_bin_get_property (GObject * object, guint prop_id,
 static void
 gst_http_ext_bin_finalize (GObject * self)
 {
+  GstHttpExtBin *bin = GST_HTTP_EXT_BIN (self);
+
+  if (bin->uri)
+    g_free (bin->uri);
   G_OBJECT_CLASS (parent_class)->finalize (self);
 }
 
@@ -125,6 +161,28 @@ static gboolean
 gst_http_ext_bin_uri_set_uri (GstURIHandler * handler, const gchar * uri,
     GError ** error)
 {
+  gboolean ret = TRUE;
+  GstHttpExtBin *bin;
+  GstState current_state;
+
+  if (!uri) {
+    GST_WARNING_OBJECT (bin, "uri to set is empty");
+    return FALSE;
+  }
+
+  bin = GST_HTTP_EXT_BIN (handler);
+  current_state = GST_STATE (bin);
+
+  /* check bin's state */
+  if (current_state == GST_STATE_PAUSED || current_state == GST_STATE_PLAYING) {
+    GST_WARNING_OBJECT (bin, "state of bin is %s",
+        gst_element_state_get_name (current_state));
+    return FALSE;
+  }
+
+  /* set original uri */
+  g_free (bin->uri);
+  bin->uri = g_strdup (uri);
   return TRUE;
 }
 
